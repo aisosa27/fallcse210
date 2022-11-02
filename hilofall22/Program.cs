0dad93b03c5d2c8c4139e02e582481c50eb6950a
aisosa27 <91696727+aisosa27@users.noreply.github.com>
2022-11-01 21:43:30 -0600

    Update Program.cs

--- hilofall22/Program.cs
+++ hilofall22/Program.cs
@@ -1,17 +1,16 @@
 ﻿using hilo;
 
 
-namespace hilo
+namespace hilo_game
 {
-    /// <summary>
+    
     /// The program's entry point.
-    /// </summary>
+    
     class Program
     {
-        /// <summary>
+        
         /// Starts the program using the given arguments.
-        /// </summary>
-        /// <param name="args">The given arguments.</param>
+      
         static int Main(string[] args)
         {
             Director director = new Director();
